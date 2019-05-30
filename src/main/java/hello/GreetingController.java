bb4b76c5233236f5414e8b6c41f3fef5a67dfad4
Dhananjay Kadam <kadamdhananjay@johndeere.com>
2019-05-30 11:07:09 +0530

    push from server

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,8 +6,6 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import java.util.stream.IntStream;
-
 @Controller
 public class GreetingController {
 
@@ -16,11 +14,8 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public void greeting(HelloMessage message) throws Exception {
-//        Thread.sleep(1000); // simulated delay
-//        return new Greeting("Hello, " + message.getName() + "!");
-        IntStream.range(1,10).forEach(i-> {
-            template.convertAndSend("/topic/greetings", new Greeting("Hello, " + message.getName() + "!"));
-        });
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
     }
 }
